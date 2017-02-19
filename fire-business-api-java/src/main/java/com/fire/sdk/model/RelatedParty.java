56b0730ef4cbec5f9657ab758add9ead5b9b3e50
Owen O Byrne <owen.obyrne@iol.ie>
2017-02-19 22:12:43 +0000

    Added RelatedParty Users

--- fire-business-api-java/src/main/java/com/fire/sdk/model/RelatedParty.java
+++ fire-business-api-java/src/main/java/com/fire/sdk/model/RelatedParty.java
@@ -1,17 +1,17 @@
 package com.fire.sdk.model;
 
 public class RelatedParty {
-    private String type;
+    private RelatedParty.Type type;
     private RelatedParty.Account account;
+    private RelatedParty.User user;
 
-    public String getType() {
+    public RelatedParty.Type getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(RelatedParty.Type type) {
         this.type = type;
     }
-    
     
     public RelatedParty.Account getAccount() {
 		return account;
@@ -20,7 +20,49 @@ public class RelatedParty {
 	public void setAccount(RelatedParty.Account account) {
 		this.account = account;
 	}
+	
+	public RelatedParty.User getUser() {
+		return user;
+	}
 
+	public void setUser(RelatedParty.User user) {
+		this.user = user;
+	}
+	
+
+	public enum Type {
+		FIRE_ACCOUNT,
+		EXTERNAL_ACCOUNT,
+		WITHDRAWAL_ACCOUNT,
+		USER
+	}
+
+
+	public class User {
+		private String alias;
+		private String mobilePhoneNumber;
+		private String imageUrl;
+		public String getAlias() {
+			return alias;
+		}
+		public void setAlias(String alias) {
+			this.alias = alias;
+		}
+		public String getMobilePhoneNumber() {
+			return mobilePhoneNumber;
+		}
+		public void setMobilePhoneNumber(String mobilePhoneNumber) {
+			this.mobilePhoneNumber = mobilePhoneNumber;
+		}
+		public String getImageUrl() {
+			return imageUrl;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		
+		
+	}
 
 	public class Account {
     	private Long id;
