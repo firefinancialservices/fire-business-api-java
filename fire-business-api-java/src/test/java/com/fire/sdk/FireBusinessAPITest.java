9c883781017f964d46b5561378cfc5cbe764e063
Owen O Byrne <owen.obyrne@iol.ie>
2018-04-04 00:45:39 +0100

    Tweaks to test

--- fire-business-api-java/src/test/java/com/fire/sdk/FireBusinessAPITest.java
+++ fire-business-api-java/src/test/java/com/fire/sdk/FireBusinessAPITest.java
@@ -1,14 +1,19 @@
 package com.fire.sdk;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fire.sdk.model.Credentials;
-import com.fire.sdk.model.request.TransactionRequest;
-import com.fire.sdk.model.response.TransactionResponse;
+import com.fire.sdk.model.request.AccountListRequest;
+import com.fire.sdk.model.request.AccountTransactionListRequest;
+import com.fire.sdk.model.response.AccountListResponse;
+import com.fire.sdk.model.response.AccountTransactionListResponse;
 
 public class FireBusinessAPITest {
 
@@ -37,16 +42,27 @@ public class FireBusinessAPITest {
 		logger.debug("ClientId {}", prop.getProperty("clientId"));
 		FireBusinessAPI api = new FireBusinessAPI().initialise(credentials);  
 		
-//		AccountListResponse accountList = api.send(new AccountListRequest());
-//		Long accountId = accountList.getAccounts().get(0).getIcan();
+		AccountListResponse accountList = api.send(new AccountListRequest());
+		Long accountId = accountList.getAccounts().get(0).getIcan();
 //		AccountResponse account = api.send(new AccountRequest().setAccountId(accountId));
 //		
 //        logger.info("Account details: {}, {} / {}", account.getName(), account.getCbic(), account.getCiban());
 //		
-//        AccountTransactionListResponse transactions = api.send(new AccountTransactionListRequest().setAccountId(accountId));
-//        logger.info("Transaction 0 details: {}", transactions.getTransactions().get(0).getType());
+		AccountTransactionListResponse transactions = null;
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			transactions = api.send(new AccountTransactionListRequest()
+					.setAccountId(accountId)
+					.setDateRangeFrom(df.parse("2017-05-01"))
+					.setDateRangeTo(df.parse("2017-06-01"))
+					);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        logger.info("Transaction 0 details: {}", transactions.getTransactions().get(0).getType());
         
-        TransactionResponse transaction = api.send(new TransactionRequest().setTransactionId(203162l));
+//        TransactionResponse transaction = api.send(new TransactionRequest().setTransactionId(203162l));
         
 		
 	}
